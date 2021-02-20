2d7eaa224ad71eb39910f4a5e32e50ff3f0b4c30
LeRon Atkins <leatkins@aboveall-media.tech>
2021-02-20 13:36:25 -0600

    Repaired worried className

--- src/ScriptureOutput8.js
+++ src/ScriptureOutput8.js
@@ -71,7 +71,7 @@ function ScriptureOutput8(BibleText, scriptureReference){
         <div>
            
         <br/><br/>
-        <div>
+        <div className="App Content Appheader1">
         <h1>If you are <strong>Worried</strong>, remember these words....</h1>
             
             <br/>
